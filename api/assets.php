234a743931f85615f0b8ed694ed16cb5f2fe54e2
Matt Goldsworthy <matt.j.goldsworthy@gmail.com>
2023-10-28 00:32:32 -0600

    Update assets.php

--- api/assets.php
+++ api/assets.php
@@ -12,13 +12,13 @@ if (str_ends_with($file, '.css')) {
 } elseif (str_ends_with($file, '.json')) {
     header('Content-Type: application/json; charset: UTF-8');
 
-    return require dirname(__DIR__).'/public/json/'.basename($file);
+    require __DIR__.'/../public/json/'.$file;
 } elseif (str_ends_with($file, 'svg')) {
     header('Content-Type: image/svg+xml; charset: UTF-8');
 
-    return require dirname(__DIR__).'/public/img/'.basename($file);
+    require __DIR__.'/../public/img/'.$file;
 } elseif (str_ends_with($file, 'jpg')) {
     header('Content-Type: image/jpeg; charset: UTF-8');
 
-    return require dirname(__DIR__).'/public/img/'.basename($file);
+    require __DIR__.'/../public/img/'.$file;
 }
